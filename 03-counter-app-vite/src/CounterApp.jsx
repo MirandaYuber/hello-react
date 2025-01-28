3606aba053c348dbc0376c513b644cf53926d5b5
MirandaYuber <mirandayuber7@gmail.com>
2025-01-27 23:43:12 -0500

    Avances evento click

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types"
 
+
 // Usar rafc + Tab para que cree la estructura de un fuction component
 export const CounterApp = ({ value }) => {
+    // Las funciones que no tengan interacción con el componente 
+    // se pueden declarar fuera del componente
+    const handleAdd = (value) => {
+        console.log(value);
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2> { value } </h2>
+            <button onClick={ handleAdd(value) }>
+                +1
+            </button>
         </>
     )
 }
