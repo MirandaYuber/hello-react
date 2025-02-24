b9de85240e9b278054d87698d9450ea04ee4951c
yuber.mojica <yuber.miranda@savne.net>
2025-02-24 16:42:49 -0500

    Ajustes test para el archivo 11-async-await

--- 03-counter-app-vite/test/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/test/base-pruebas/11-async-await.test.js
@@ -4,7 +4,9 @@ describe('Pruebas en 11-aync-await', () => {
     test('getImagen debe retornar un error si no tenemos api key', async() => {
 
         const response = await getImagen()
+                
         expect(typeof response).toBe('string')
+        expect(response).toBe('No existe la imagen')
         // expect(response.includes('http')).toBe(true)
 
     })
