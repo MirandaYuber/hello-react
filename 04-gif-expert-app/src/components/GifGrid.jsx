c542b0cb4662ba08eafdff7fba30ae15dc9db6aa
MirandaYuber <mirandayuber7@gmail.com>
2025-04-13 16:13:18 -0500

    Se listan todos los gifs por categorias

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -2,12 +2,20 @@ import { useState, useEffect } from "react"
 import { getGifs } from "../helpers/getGifs"
 
 const GifGrid = ({ category }) => {
+  
   const [counter, setCounter] = useState(0)
+  const [images, setImages] = useState([])
+
+  const getImages = async() => {
+    const newImages = await getGifs(category)
+    setImages(newImages)
+  }
 
   // El useEffect se utiliza para disparar efectos secundarios 
   // (proceso a ejecutar cuando algo suceda) en componentes funcionales
+  // El solo debe recibir una función, no una promesa
   useEffect(() => {
-    getGifs(category)
+    getImages()    
   }, []) 
   // El segundo argumento es un array de dependencias, si no se pasa, se ejecuta cada vez que el componente se renderiza
   // Si se pasa un array vacio, se ejecuta una sola vez al cargar el componente
@@ -16,8 +24,20 @@ const GifGrid = ({ category }) => {
   return (
     <div>
       <h3>{ category }</h3>
-      <p>{counter}</p>
-      <button onClick={() => setCounter(counter + 1)}> +1 </button>
+
+      <ol>
+        {
+          images.map(({id, title, url}) => {
+            if (title === '') title = `${category}`
+
+            return(
+              <li key={id}>
+                <a href={url} target="_blank">{title}</a>
+              </li>
+            )
+          })
+        }
+      </ol>
     </div>
   )
 }
--- 04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/src/helpers/getGifs.js
@@ -2,7 +2,6 @@ export const getGifs = async (category) => {
     const key = 'iKLd3jFD8pf3ipEVtNesIAGzMfJFbc7d'
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${category}&limit=10`
     
-
     const response = await fetch(url)
     const {data} = await response.json()
 
